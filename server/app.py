ca61b2e39d18f84dcb0c48fa6ab71ab09e184c7a
Michael McCormack <mcctwin@gmail.com>
2026-01-07 13:54:59 -0600

    all tests passed

--- server/app.py
+++ server/app.py
@@ -84,15 +84,23 @@ class CheckSession(Resource):
         
         return {}, 401
 
+@app.before_request
+def check_if_logged_in():
+    if not session.get('user_id') and request.endpoint in ['member_index', 'member_article']:
+        return {'message': 'You must be logged in to view this article'}, 401
+
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = [ArticleSchema().dump(article) for article in Article.query.filter(Article.is_member_only == True).all()]
+        return make_response(articles, 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        article_json = ArticleSchema().dump(article)
+        return make_response(article_json, 200)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
